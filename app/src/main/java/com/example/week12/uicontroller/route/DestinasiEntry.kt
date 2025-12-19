0fe95b162c68dbde868375757afe3127db2e77ef
ucup233 <sufhidayat9@gmail.com>
2025-12-19 17:02:05 +0700

    Pembuatan Destinasi Entry

---
+++ app/src/main/java/com/example/week12/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.week12.uicontroller.route
+
+import com.example.week12.R
+
+object DestinasiEntry: DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
