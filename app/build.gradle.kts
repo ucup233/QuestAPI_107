0713cdfcb9897c7b6563729fecb2a62e840a9d2c
ucup233 <sufhidayat9@gmail.com>
2025-12-19 17:10:08 +0700

    Pembuatan Halaman Entry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
+    implementation(libs.androidx.material3)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
