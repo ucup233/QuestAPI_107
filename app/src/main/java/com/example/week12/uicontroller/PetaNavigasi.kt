0259540bbc21a4623d5a01db1ff959d3866abaf7
ucup233 <sufhidayat9@gmail.com>
2025-12-19 17:20:06 +0700

    Pembuatan Peta Navigasi

---
+++ app/src/main/java/com/example/week12/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,51 @@
+package com.example.week12.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.week12.uicontroller.route.DestinasiEntry
+import com.example.week12.uicontroller.route.DestinasiHome
+import com.example.week12.uicontroller.route.DestinasiDetail
+import com.example.week12.view.EntrySiswaScreen
+import com.example.week12.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
