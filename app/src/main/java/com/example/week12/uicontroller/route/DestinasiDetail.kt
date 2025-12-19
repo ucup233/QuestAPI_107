176850f208199c7310a2a5c7d5351f2f59cf75b0
ucup233 <sufhidayat9@gmail.com>
2025-12-19 17:01:00 +0700

    Pembuatan Destinasi Detail

---
+++ app/src/main/java/com/example/week12/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,8 @@
+package com.example.week12.uicontroller.route
+
+import com.example.week12.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = R.string.detail_siswa
+}
