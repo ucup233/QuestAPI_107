d758313c522d2f9c32f79a1427ffaf4a80cef2d0
ucup233 <sufhidayat9@gmail.com>
2025-12-19 17:22:11 +0700

    Pembuatan Navigasi

--- app/src/main/java/com/example/week12/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/week12/uicontroller/route/DestinasiDetail.kt
@@ -2,6 +2,7 @@ package com.example.week12.uicontroller.route
 
 import com.example.week12.R
 
+
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "item_detail"
     override val titleRes = R.string.detail_siswa
