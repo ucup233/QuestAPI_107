d269a67c913d60b553fc592515a00a8f1fcceff0
ucup233 <sufhidayat9@gmail.com>
2025-12-19 17:02:50 +0700

    Pembuatan Destinasi Home

---
+++ app/src/main/java/com/example/week12/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.week12.uicontroller.route
+
+import com.example.week12.R
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = R.string.nama
+}
