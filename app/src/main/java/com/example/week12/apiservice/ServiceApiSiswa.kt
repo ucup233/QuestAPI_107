6d13e482efc297f7f4e9e44dd382be05aeb07330
ucup233 <sufhidayat9@gmail.com>
2025-12-26 14:49:34 +0700

    Penambahan fungsi delete 1 siswa di api service

--- app/src/main/java/com/example/week12/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/week12/apiservice/ServiceApiSiswa.kt
@@ -2,6 +2,7 @@ package com.example.week12.apiservice
 
 import com.example.week12.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -21,6 +22,6 @@ interface ServiceApiSiswa {
     @PUT("editTM.php/{id}")
     suspend fun editSatuSiswa(@Query("id") id: Int, @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 
-    //@DELETE("deleteTM.php/{id}")
-    //suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
 }
