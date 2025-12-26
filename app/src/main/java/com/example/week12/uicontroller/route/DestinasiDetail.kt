e5f9139be839b3cf0c84bc97308d94e2a512c25f
ucup233 <sufhidayat9@gmail.com>
2025-12-26 14:50:40 +0700

    Update destinasi detail

--- app/src/main/java/com/example/week12/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/week12/uicontroller/route/DestinasiDetail.kt
@@ -6,4 +6,6 @@ import com.example.week12.R
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "item_detail"
     override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idsiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
